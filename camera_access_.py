4aa0a45efc36dc2995e8f0042545c658324bd879
alaa-hatoum <alaa.hatoum@mail.utoronto.ca>
2022-07-03 20:27:16 -0400

    updated it to save the last frame of the video

--- camera_access_.py
+++ camera_access_.py
@@ -1,18 +1,34 @@
 # import the opencv library
 import cv2
 
+#importing os module
+import os
+
+#Image name 
+filename = 'test.jpg'
+
+#image directory
+
+directory = r'C:\Users\alaah\OneDrive\Desktop\Personal Software Projects\The-Magic-Finger'
+
 vid = cv2.VideoCapture(0)
 
 while(True):
     #capture the video frame by frame
     ret, frame = vid.read()
     
-    #displat the resulting frame
+    #display the resulting frame
     cv2.imshow('Reflection', frame)
+    
     
     #makes the loop end if you press 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+#changes location of directory
+os.chdir(directory)
+
+#saves the last frame of the video
+cv2.imwrite(filename, frame)
 
 # After the loop release the cap object 
 vid.release()
